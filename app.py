4af0c5aec28216eb5245cdaf8094fe34fdebcce5
Book-reader <89329785+Book-reader@users.noreply.github.com>
2023-10-27 21:33:32 +1300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,55 @@
+# importing modules from flask library
+from flask import Flask , render_template
+
+# creating instance of class Flask, by providing __name__ keyword as argument
+app = Flask(__name__)
+
+# write the routes using decorator functions
+# default route or 'URL'
+@app.route("/")
+def home():
+
+    name = "Samuel" # write your name
+    age = "15" # write your age
+
+    return render_template('index.html' , name = name , age = age)
+
+# define the route to father webpage
+@app.route("/father")
+
+def father_page():
+    name = "James"
+    age = "55"
+
+    return render_template('father.html' , name = name , age = age)
+
+
+# define the route to mother webpage
+@app.route("/mother")
+
+def mother_page():
+    name = "Sonya"
+    age = "47"
+
+    return render_template('mother.html' , name = name , age = age)
+
+
+
+# define the route to friends webpage
+@app.route("/friends")
+
+def friends_page():
+    name = "Rowan"
+    age = "16"
+
+    return render_template('friend.html' , name = name , age = age)
+
+
+# add other routes, if you want
+
+
+
+
+# run the file
+if __name__  ==  '__main__':
+    app.run(debug=True)
